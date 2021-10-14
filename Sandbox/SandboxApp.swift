1030abcbe0aa8caa059ae1af05ff8d65c90e356f
shinter61 <shinter61@gmail.com>
2021-10-15 04:22:51 +0900

    Initial Commit

---
+++ Sandbox/SandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  SandboxApp.swift
+//  Sandbox
+//
+//  Created by 松本真太朗 on 2021/10/15.
+//
+
+import SwiftUI
+
+@main
+struct SandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
